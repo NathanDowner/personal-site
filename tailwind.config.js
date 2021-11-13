794a3b34e6ac7d85ba62a4d14ff55e798012253c
Nathan Downer <nathandowner123@gmail.com>
2021-11-12 21:35:21 -0500

    add project colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'bg-blue': '#0B1120',
+      'red': '#F60512',
+      'light': '#ffffff',
+      'card-bg': '#111A32'
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'system-ui', 'sans-serif'],
+      'mono': ['"Fira Code"', 'mono']
+    },
     extend: {},
   },
   variants: {
