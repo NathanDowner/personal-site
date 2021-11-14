f3e1ee3cd550ae2f8c88b8c0f37ad7c1e4fbc905
Nathan Downer <nathandowner123@gmail.com>
2021-11-13 21:39:39 -0500

    add prettier config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,30 @@
-import logo from "../assets/Logo.svg";
+import logo from '../assets/Logo.svg';
 
 const Header = () => {
   return (
-    <header className="absolute w-full mx-auto max-w-screen-xl flex items-center justify-between py-5 px-5 ">
+    <header className="sticky z-10 top-0 max-w-screen-xl mx-auto flex items-center justify-between py-5 px-5 ">
       <img src={logo} alt="Logo" />
       <nav>
         <ul className="flex items-center space-x-6">
-          <li><a href="/">About</a></li>
-          <li><a href="/">Work</a></li>
-          <li><a href="/">Experience</a></li>
-          <li><a href="/">Contact</a></li>
-          <li><button className="px-2 py-1 rounded-md bg-red" >Resume</button></li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Work</a>
+          </li>
+          <li>
+            <a href="/">Experience</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <button className="px-2 py-1 rounded-md bg-red">Resume</button>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
