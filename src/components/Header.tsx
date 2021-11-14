120a17faff454ce72212619f1bfac7c432908739
Nathan Downer <nathandowner123@gmail.com>
2021-11-13 22:06:39 -0500

    update landing bg image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/Logo.svg';
 
 const Header = () => {
   return (
-    <header className="sticky z-10 top-0 max-w-screen-xl mx-auto flex items-center justify-between py-5 px-5 ">
+    <header className="sticky z-10 top-0 max-w-screen-2xl mx-auto flex items-center justify-between py-5 px-5 ">
       <img src={logo} alt="Logo" />
       <nav>
         <ul className="flex items-center space-x-6">
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,13 +6,13 @@ import linkedIn from '../assets/icons/linkedin-icon.svg';
 const Landing = () => {
   return (
     <section className="relative h-screen -mt-20">
-      <div className="absolute top-0 left-0 w-full h-full bg-landing-img background-clip bg-cover  bg-right-4 bg-no-repeat" />
+      <div className="absolute right-0 w-2/3 h-full bg-landing-img background-clip bg-cover bg-right bg-no-repeat" />
 
-      <div className="max-w-screen-xl mx-auto">
+      <div className="mx-auto max-w-screen-2xl">
         <div className="absolute z-30 top-1/2 pl-5 transform -translate-y-1/2">
           <h3 className="font-mono text-xl">Hi there</h3>
-          <h1 className="text-6xl font-bold">I'm Nathan Downer</h1>
-          <h2 className="text-xl">
+          <h1 className="text-6xl font-bold my-2">I'm Nathan Downer</h1>
+          <h2 className="text-2xl">
             A Software Engineer who loves{' '}
             <span className="font-bold">
               Frontend <br /> Development
