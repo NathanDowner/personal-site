fa5ca9757f2bb972abd0c9124bae238e773586b6
Nathan Downer <nathandowner123@gmail.com>
2021-11-21 12:36:11 -0500

    use tailwind variabts to layout the job cards

--- src/components/JobEntry.tsx
+++ src/components/JobEntry.tsx
@@ -1,14 +1,6 @@
-type Props = {
-  isReversed: boolean;
-};
-
-const JobEntry = ({ isReversed }: Props) => {
+const JobEntry = () => {
   return (
-    <li
-      className={`${
-        isReversed && 'reversed'
-      } grid grid-cols-job-entry gap-6 items-center`}
-    >
+    <li className="odd:reversed grid grid-cols-job-entry gap-6 items-center">
       <p className="justify-self-end">Jan - Feb 2021</p>
       <div className="work-blip relative rounded-full h-4 w-4 bg-red" />
       <div className="relative not-reversed p-6 bg-card-bg inline-block rounded-lg shadow-md">
--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -6,9 +6,9 @@ const WorkSection = () => {
     <FullPageSection title="Where I've Worked" watermarkText="Work">
       <div className="flex justify-center items-center ">
         <ul className="relative space-y-4 mt-10">
-          <div className="absolute border-l-4 border-dashed border-red h-full left-1/2 transform -translate-x-1/2" />
-          {[1, 2, 3, 4].map((blank, index) => (
-            <JobEntry key={blank} isReversed={index % 2 === 0} />
+          <div className="absolute border-l-4 border-red h-full left-1/2 transform -translate-x-1/2" />
+          {[1, 2, 3, 4].map((blank) => (
+            <JobEntry key={blank} />
           ))}
         </ul>
       </div>
