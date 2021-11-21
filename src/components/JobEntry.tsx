a5bff7ad52a22a97197d9fa8c65df001e140369e
Nathan Downer <nathandowner123@gmail.com>
2021-11-21 12:46:31 -0500

    increase border radius

--- src/components/JobEntry.tsx
+++ src/components/JobEntry.tsx
@@ -3,7 +3,7 @@ const JobEntry = () => {
     <li className="odd:reversed grid grid-cols-job-entry gap-6 items-center">
       <p className="justify-self-end">Jan - Feb 2021</p>
       <div className="work-blip relative rounded-full h-4 w-4 bg-red" />
-      <div className="relative not-reversed p-6 bg-card-bg inline-block rounded-lg shadow-md">
+      <div className="relative not-reversed p-6 bg-card-bg inline-block rounded-xl shadow-md">
         <h3 className="text-2xl font-bold text-red">Software Engineer</h3>
         <h4 className="text-xl font-medium">Speur Professional Services</h4>
         <ul className="text-sm list-disc ml-6">
