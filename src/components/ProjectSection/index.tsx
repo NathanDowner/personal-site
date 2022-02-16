6a8806bffc2104484d71baa391a9986da12b5c4e
Nathan Downer <nathandowner123@gmail.com>
2022-02-16 02:03:48 -0500

    add coming soon to project section

--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -6,7 +6,10 @@ const ProjectsSection = () => {
   return (
     <FullPageSection id="projects" watermarkText="Projects">
       <SectionTitle title="Things I've Built" />
-      <div className="flex w-full max-w-screen-2xl items-center justify-evenly">
+      <div className="absolute inset-0 z-20 grid select-none place-items-center text-center text-4xl font-bold backdrop-blur-md lg:text-6xl">
+        Project Section Coming Soon!!!
+      </div>
+      <div className="relative flex w-full max-w-screen-2xl items-center justify-evenly">
         {/* Billboard */}
         <div className="relative left-5 transform [perspective:1000px] [perspective-origin:60%_50%]">
           <h3 className="font-bold [transform:rotateY(20deg)]">
